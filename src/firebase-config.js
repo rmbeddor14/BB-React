0de53dbc8a0478eb69df0a32a803559fb0c9da3e
Rachel Beddor <rmbeddor@gmail.com>
2024-10-23 18:47:07 -0700

    moved api keys to .env and added .env to .gitignore to prevent keys from exposure

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -14,13 +14,13 @@ import {getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBpFLVUg2SE8uxSVcTfGl9us0P5DnzbD4w",
-  authDomain: "bb-react-f3374.firebaseapp.com",
-  projectId: "bb-react-f3374",
-  storageBucket: "bb-react-f3374.appspot.com",
-  messagingSenderId: "753372043484",
-  appId: "1:753372043484:web:bc8c7eb6586a8dd5b07c19",
-  measurementId: "G-KB4GCRZ9KZ"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // // Initialize Firebase
